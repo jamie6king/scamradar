d64c9d1a1ad6466fe165cb44d62e645cd095edaf
Jamie <jamieplumy@gmail.com>
2024-12-11 15:04:36 +0000

    Make lint happy

--- api/tests/test-responses/DVLA-VES-API.js
+++ api/tests/test-responses/DVLA-VES-API.js
@@ -1,40 +1,38 @@
 const response200MockJSON = {
-    "registrationNumber": "AA19AAA",
-    "artEndDate": "2025-03-30",
-    "co2Emissions": 300,
-    "engineCapacity": 2000,
-    "euroStatus": "EURO1",
-    "markedForExport": false,
-    "fuelType": "PETROL",
-    "motStatus": "No details held by DVLA",
-    "revenueWeight": 0,
-    "colour": "RED",
-    "make": "FORD",
-    "typeApproval": "M1",
-    "yearOfManufacture": 2019,
-    "taxDueDate": "<1 YEAR FROM NOW>",
-    "taxStatus": "Taxed",
-    "dateOfLastV5CIssued": "2019-05-20",
-    "wheelplan": "2 AXLE RIGID BODY",
-    "monthOfFirstDvlaRegistration": "2019-03",
-    "monthOfFirstRegistration": "2019-03",
-    "realDrivingEmissions": "1"
-}
+    registrationNumber: "AA19AAA",
+    artEndDate: "2025-03-30",
+    co2Emissions: 300,
+    engineCapacity: 2000,
+    euroStatus: "EURO1",
+    markedForExport: false,
+    fuelType: "PETROL",
+    motStatus: "No details held by DVLA",
+    revenueWeight: 0,
+    colour: "RED",
+    make: "FORD",
+    typeApproval: "M1",
+    yearOfManufacture: 2019,
+    taxDueDate: "<1 YEAR FROM NOW>",
+    taxStatus: "Taxed",
+    dateOfLastV5CIssued: "2019-05-20",
+    wheelplan: "2 AXLE RIGID BODY",
+    monthOfFirstDvlaRegistration: "2019-03",
+    monthOfFirstRegistration: "2019-03",
+    realDrivingEmissions: "1",
+};
 
 const response404MockJSON = {
-    "errors": [
+    errors: [
         {
-            "status": "404",
-            "code": "404",
-            "title": "Vehicle Not Found",
-            "detail": "Record for vehicle not found"
-        }
-    ]
-}
+            status: "404",
+            code: "404",
+            title: "Vehicle Not Found",
+            detail: "Record for vehicle not found",
+        },
+    ],
+};
 
 module.exports = {
     response200MockJSON,
-    response404MockJSON
+    response404MockJSON,
 };
-
-
