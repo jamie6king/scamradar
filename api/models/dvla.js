f7acd61b37e05d4b1b446c38c6234305de40a745
Abdirahman <mraaden10@gmail.com>
2024-12-11 15:59:18 +0000

    dvla/tests

--- api/models/dvla.js
+++ api/models/dvla.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
 
 const DvlaResponseSchema = new mongoose.Schema({
-    productId: {type: String, required: true, unique: true},
-    response: {type: Object, required: true},
+    productId: { type: String, required: true, unique: true },
+    response: { type: Object, required: true },
 });
 
 const DvlaResponse = mongoose.model("DvlaResponse", DvlaResponseSchema);
 
-
-module.exports = DvlaResponse
+module.exports = DvlaResponse;
