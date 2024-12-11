be3752a0b2ed125f3fc77fda7c7918a2cf7a6f94
Abdirahman <mraaden10@gmail.com>
2024-12-11 14:53:59 +0000

    schmea/model for DVLA api

---
+++ api/models/dvla.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const DvlaResponseSchema = new mongoose.Schema({
+    productId: {type: String, required: true, unique: true},
+    response: {type: Object, required: true},
+});
+
+const DvlaResponse = mongoose.model("DvlaResponse", DvlaResponseSchema);
+
+
+module.exports = DvlaResponse
---
+++ api/tests/models/dvla.test.js
@@ -0,0 +1,37 @@
+const DvlaResponse = require("../../models/dvla")
+
+
+describe("dvlaResponse model", () => {
+ it("should create a DVLAResponse instance", async () => {
+        const record = new DvlaResponse({
+          productId: "TEST123",
+          response: {
+            registrationNumber: "AB12CDE",
+            taxStatus: "TAXED",
+            taxDueDate: "2025-06-09",
+            motStatus: "Valid",
+            make: "AUDI",
+            yearOfManufacture: 2014,
+            engineCapacity: 1969,
+            co2Emissions: 109,
+            fuelType: "DIESEL",
+            markedForExport: false,
+            colour: "GREEN",
+            typeApproval: "M1",
+            revenueWeight: 1870,
+            dateOfLastV5CIssued: "2024-06-27",
+            motExpiryDate: "2025-07-08",
+            wheelplan: "2 AXLE RIGID BODY",
+            monthOfFirstRegistration: "2013-12"
+
+          },
+        });
+        expect(record.productId).toEqual("TEST123");
+        expect(record.response.registrationNumber).toEqual("AB12CDE");
+        expect(record.response.make).toEqual("AUDI");
+        expect(record.response.fuelType).toEqual("DIESEL");
+        expect(record.response.make).toEqual("AUDI");
+        expect(record.response.monthOfFirstRegistration).toEqual("2013-12");
+      });
+
+    })
