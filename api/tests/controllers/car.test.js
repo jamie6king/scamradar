9fc997035d6824219b828c3a9b06cb77a1bc4a38
Jamie <jamieplumy@gmail.com>
2024-12-12 14:43:16 +0000

    added POST /car route with carReport function. Will return 400 if request format is incorrect, 200 otherwise

--- api/tests/controllers/car.test.js
+++ api/tests/controllers/car.test.js
@@ -15,4 +15,44 @@ describe("/car", () => {
             expect(response.body).toEqual({ message: "Car route test" });
         });
     });
+
+    describe("POST / with malformed registrationNumber in request JSON", () => {
+        test("the response code is 400", async () => {
+            const response = await request(app)
+                .post("/car")
+                .send({ registrationber: "AA19AAA" });
+
+            expect(response.statusCode).toBe(400);
+        });
+
+        test("Returns error message", async () => {
+            const response = await request(app)
+                .post("/car")
+                .send({ registrationber: "AA19AAA" });
+
+            expect(response.body).toEqual({
+                message:
+                    "Couldn't find registrationNumber, did you send this in the request body?",
+            });
+        });
+    });
+    describe("POST / with valid registrationNumber in request JSON", () => {
+        test("the response code is 400", async () => {
+            const response = await request(app)
+                .post("/car")
+                .send({ registrationNumber: "AA19AAA" });
+
+            expect(response.statusCode).toBe(200);
+        });
+
+        test("Returns error message", async () => {
+            const response = await request(app)
+                .post("/car")
+                .send({ registrationNumber: "AA19AAA" });
+
+            expect(response.body).toEqual({
+                message: "Great success!!!!",
+            });
+        });
+    });
 });
