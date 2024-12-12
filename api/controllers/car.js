0c07d7a4cd98f13204c7e2be83721264697a7096
Jamie <jamieplumy@gmail.com>
2024-12-12 17:03:07 +0000

    car.js carReport produces a report, tests added

--- api/controllers/car.js
+++ api/controllers/car.js
@@ -1,17 +1,38 @@
+const getDvlaJson = require("../services/car");
+
 function test(req, res) {
     res.status(200).json({ message: "Car route test" });
 }
 
-function carReport(req, res) {
+async function carReport(req, res) {
     const registrationNumber = req.body.registrationNumber;
     if (!registrationNumber) {
-        res.status(400).json({
+        return res.status(400).json({
             message:
                 "Couldn't find registrationNumber, did you send this in the request body?",
         });
     }
-    res.status(200).json({
-        message: "Great success!!!!",
+    const vehicleData = req.body.vehicleData;
+    if (!vehicleData) {
+        return res.status(200).json({
+            message: "No comparisons can be made",
+        });
+    }
+
+    const dvlaResponse = await getDvlaJson(registrationNumber);
+    const reportArray = Object.entries(vehicleData).map(([key, value]) => {
+        const dvlaResponseLowerKey = (dvlaResponse[key] || "").toLowerCase();
+        if (dvlaResponseLowerKey == value.toLowerCase()) {
+            return [key, "Pass"];
+        } else if (dvlaResponseLowerKey == "") {
+            return [key, "Fail, no data found"];
+        } else {
+            return [key, `Fail, ${key} should be: ${dvlaResponseLowerKey}`];
+        }
+    });
+    const reportObject = Object.fromEntries(reportArray);
+    return res.status(200).json({
+        message: reportObject,
     });
 }
 
