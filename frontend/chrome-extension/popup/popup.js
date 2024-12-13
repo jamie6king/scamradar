5d8d0454671d4e1173e7193b7abf42c2fce541c3
Jamie King <jamiedanielking@gmail.com>
2024-12-13 15:03:37 +0000

    Banish thy comments

--- frontend/chrome-extension/popup/popup.js
+++ frontend/chrome-extension/popup/popup.js
@@ -7,13 +7,4 @@ function submitted (event){
 	event.preventDefault();
 	const registrationNumber = event.target.textInput.value;
 	chrome.runtime.sendMessage({ type: "registrationNumber", registrationNumber: registrationNumber });
-
-	alert(event.target.textInput.value)
 }
-// document.addEventListener('DOMContentLoaded', () => {
-//     const button = document.getElementById('formSubmit');
-//     button.addEventListener('click', () => {
-//         console.log('Button clicked!');
-//         // Your function logic here
-//     });
-// });
