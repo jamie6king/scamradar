b84d9895db83b33f655f23b3143b3dae6836ddfb
Jamie King <jamiedanielking@gmail.com>
2024-12-16 14:49:04 +0000

    Hell froze over

--- frontend/chrome-extension/popup/popup.js
+++ frontend/chrome-extension/popup/popup.js
@@ -1,21 +1,27 @@
 window.onload = () => {
-	const form = document.querySelector("form")
-	form.onsubmit = submitted.bind(form)
-}
+    const form = document.querySelector("form");
+    form.onsubmit = submitted.bind(form);
+};
 
-function submitted (event){
-	event.preventDefault();
-	const registrationNumber = event.target.textInput.value;
-	chrome.runtime.sendMessage({ type: "registrationNumber", registrationNumber: registrationNumber });
-	// chrome.action.setPopup({ popup: "popup/results.html" });
-}
-
-document.addEventListener("click", function () {
-    chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-      console.log(message)  
-			if (message.type === "results") {
-            console.log(message.carReport);
-            sendResponse({ status: "success" });
-        }
+function submitted(event) {
+    event.preventDefault();
+    const registrationNumber = event.target.textInput.value;
+    chrome.runtime.sendMessage({
+        type: "registrationNumber",
+        registrationNumber: registrationNumber,
     });
+    // chrome.action.setPopup({ popup: "popup/results.html" });
+}
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.type === "results") {
+        sendResponse({ status: "success" });
+
+        const response = message.carReport.reportResults;
+
+        document.getElementById("input").classList.add("hidden");
+        document.getElementById("output").classList.remove("hidden");
+
+        document.getElementById("make").innerText = "Make: " + response.make;
+    }
 });
