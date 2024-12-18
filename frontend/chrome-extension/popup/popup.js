0d2f2cf7689cca10bbe34a6a8cb761c10611a539
Joshua Blackmore <joshuablackmore@gmail.com>
2024-12-18 12:13:17 +0000

    added: accordions and simple loading state

--- frontend/chrome-extension/popup/popup.js
+++ frontend/chrome-extension/popup/popup.js
@@ -6,6 +6,7 @@ window.onload = () => {
 function submitted(event) {
     event.preventDefault();
     const registrationNumber = event.target.textInput.value;
+    document.getElementById("loading").classList.remove("hidden");
     chrome.runtime.sendMessage({
         type: "registrationNumber",
         registrationNumber: registrationNumber,
@@ -18,20 +19,28 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         sendResponse({ status: "success" });
 
         const response = message.carReport.reportResults;
-
+        document.getElementById("loading").classList.add("hidden");
         document.getElementById("input").classList.add("hidden");
         document.getElementById("output").classList.remove("hidden");
 
         document.getElementById("make").innerText = "Make:\n" + response.make;
-        document.getElementById("model").innerText = "Model:\n" + response.model;
-        document.getElementById("colour").innerText = "Colour:\n" + response.colour;
-        document.getElementById("fuel-type").innerText = "Fuel Type:\n" + response.fuelType;
-        document.getElementById("registration-date").innerText = "Registration Date:\n" + response.registrationDate;
-        document.getElementById("mileage").innerText = "Mileage:\n" + response.mileage;
-        document.getElementById("tax-status").innerText = "Tax Status:\n" + response.taxStatus;
-        document.getElementById("outstanding-recall").innerText = "Outstanding Recalls:\n" + response.hasOutstandingRecall;
-				// document.getElementById("mot-required").innerText = "MOT Required:\n" + (response.motRequired ? "Yes" : "No");
-        document.getElementById("mot-due").innerText = "MOT Due Date:\n" + response.motTestDueDate;
+        document.getElementById("model").innerText =
+            "Model:\n" + response.model;
+        document.getElementById("colour").innerText =
+            "Colour:\n" + response.colour;
+        document.getElementById("fuel-type").innerText =
+            "Fuel Type:\n" + response.fuelType;
+        document.getElementById("registration-date").innerText =
+            "Registration Date:\n" + response.registrationDate;
+        document.getElementById("mileage").innerText =
+            "Mileage:\n" + response.mileage;
+        document.getElementById("tax-status").innerText =
+            "Tax Status:\n" + response.taxStatus;
+        document.getElementById("outstanding-recall").innerText =
+            "Outstanding Recalls:\n" + response.hasOutstandingRecall;
+        // document.getElementById("mot-required").innerText = "MOT Required:\n" + (response.motRequired ? "Yes" : "No");
+        document.getElementById("mot-due").innerText =
+            "MOT Due Date:\n" + response.motTestDueDate;
         // document.getElementById("mot-data").innerText = "MOT Failures:\n" + response.motFailures.map((failure) => failure);
     }
 });
