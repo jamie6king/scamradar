8105a8b383c1b5057a665abb845b070de5e0ae95
Joshua Blackmore <joshuablackmore@gmail.com>
2024-12-18 15:32:57 +0000

    added: review bars

--- frontend/chrome-extension/popup/popup.js
+++ frontend/chrome-extension/popup/popup.js
@@ -45,24 +45,66 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     }
 });
 
-
 document.addEventListener("DOMContentLoaded", () => {
     chrome.storage.local.get(["mapQueryResults", "mapQueryError"], (result) => {
-        if(result.mapQueryResults) {
-            console.log("Received results", result.mapQueryResults)
-            const mapReviews = result.mapQueryResults.mapReviews
-            if(mapReviews) {
-                document.getElementById("businessName").innerText = "Business Name:\n" + mapReviews.businessName;
-                document.getElementById("businessAddress").innerText = "Business Address:\n" + mapReviews.businessAddress;
-                document.getElementById("reviewsCount").innerText = "Number of reviews:\n" + mapReviews.reviewsCount;
-                document.getElementById("averageRating").innerText = "Avg. rating:\n" + mapReviews.averageRating;
-                document.getElementById("ratingSummary").innerText = "Rating summary:\n" + mapReviews.ratingSummary;
-                document.getElementById("mostRelevantReviews").innerText = "User reviews sample:\n" + mapReviews.mostRevantReviews;
+        if (result.mapQueryResults) {
+            console.log("Received results", result.mapQueryResults);
+            const mapReviews = result.mapQueryResults.mapReviews;
+            const reviewWidths = {
+                fiveStarWidth:
+                    (mapReviews.ratingSummary[4].amount /
+                        mapReviews.reviewsCount) *
+                    100,
+                fourStarWidth:
+                    (mapReviews.ratingSummary[3].amount /
+                        mapReviews.reviewsCount) *
+                    100,
+                threeStarWidth:
+                    (mapReviews.ratingSummary[2].amount /
+                        mapReviews.reviewsCount) *
+                    100,
+                twoStarWidth:
+                    (mapReviews.ratingSummary[1].amount /
+                        mapReviews.reviewsCount) *
+                    100,
+                oneStarWidth:
+                    (mapReviews.ratingSummary[0].amount /
+                        mapReviews.reviewsCount) *
+                    100,
+            };
+            if (mapReviews) {
+                document.getElementById("businessName").innerText =
+                    "Business Name:\n" + mapReviews.businessName;
+                document.getElementById("businessAddress").innerText =
+                    "Business Address:\n" + mapReviews.businessAddress;
+                document.getElementById("reviewsCount").innerText =
+                    mapReviews.reviewsCount + " reviews";
+                document.getElementById("averageRating").innerText =
+                    mapReviews.averageRating;
+
+                document.getElementById(
+                    "five-stars"
+                ).style.width = `${reviewWidths.fiveStarWidth}%`;
+                document.getElementById(
+                    "four-stars"
+                ).style.width = `${reviewWidths.fourStarWidth}%`;
+                document.getElementById(
+                    "three-stars"
+                ).style.width = `${reviewWidths.threeStarWidth}%`;
+                document.getElementById(
+                    "two-stars"
+                ).style.width = `${reviewWidths.twoStarWidth}%`;
+                document.getElementById(
+                    "one-stars"
+                ).style.width = `${reviewWidths.oneStarWidth}%`;
+
+                document.getElementById("mostRelevantReviews").innerText =
+                    "User reviews sample:\n" + mapReviews.mostRelevantReviews;
                 // chrome.storage.local.remove(['mapQueryResults']);
             } else {
-                document.getElementById("noReviewsFound").innerText = result.mapQueryResults.message;
+                document.getElementById("noReviewsFound").innerText =
+                    result.mapQueryResults.message;
             }
         }
-    })
-})
-
+    });
+});
