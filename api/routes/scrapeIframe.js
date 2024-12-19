4e8c06c02b25cc7bea404526adce423d0b60a2e3
Khalid Ham <Khalidoham@gmail.com>
2024-12-19 13:05:01 +0000

    fixed bugs on name scraping backend

---
+++ api/routes/scrapeIframe.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const { scrapeIframeController } = require("../controllers/scrapeIframe");
+
+router.get("/getIframeUrl", scrapeIframeController.getIframeUrlFromEbayUrl);
+router.get(
+    "/getBusinessInfo",
+    scrapeIframeController.getBusinessInfoFromIframeUrl
+);
+
+module.exports = router;
